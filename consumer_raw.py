e50b282ea7c1156a67fd9bfad576107b07af579a
Cesar Costa <cesar.costa.cac.1993@gmail.com>
2023-05-26 11:58:37 -0300

    Criação

---
+++ consumer_raw.py
@@ -0,0 +1,34 @@
+import pika
+
+
+def minha_callback(ch, method, properties, body):
+    print(body)
+    # print(type(body)) # class bytes
+
+
+connection_parameters = pika.ConnectionParameters(
+    host="localhost",
+    port=5672,
+    credentials=pika.PlainCredentials(
+        username="admin",
+        password="123456"
+    )
+)
+
+channel = pika.BlockingConnection(connection_parameters).channel()
+
+channel.queue_declare(
+    queue="data_queue",
+    durable=True
+)
+
+channel.basic_consume(
+    queue="data_queue",
+    auto_ack=True,
+    on_message_callback=minha_callback
+)
+
+
+print('Listen RabbitMQ on Port 5672')
+
+channel.start_consuming()
